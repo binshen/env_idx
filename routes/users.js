48651320b009b9fcff1f1e55da557e60423744b1
bin.shen <bin.shen@openx.com>
2016-08-21 09:37:19 +0800

    111

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,18 @@ var request = require('request');
 
 module.exports = function (app, mongoose, config) {
 
+    var User = mongoose.model('User');
+
     app.get('/users/', function (req, res, next) {
         var user = req.session.login_user;
-        res.render('user', { username: user.username });
+        var nickname = user.n_name == "" || user.n_name == null ? user.u_name : user.n_name;
+        res.render('user', { n_name: nickname });
     });
 
     app.get('/users/nickname', function (req, res, next) {
-        res.render('user_nickname');
+        var user = req.session.login_user;
+        var nickname = user.n_name == "" || user.n_name == null ? user.u_name : user.n_name;
+        res.render('user_nickname', { n_name: nickname, u_id: user.u_id });
     });
 
     app.get('/users/password', function (req, res, next) {
@@ -19,21 +24,22 @@ module.exports = function (app, mongoose, config) {
         res.render('user_feedback');
     });
 
-    app.post('/users/login', function (req, res, next) {
-        // var username = req.body.username;
-        // var password = req.body.password;
-        // request.post({
-        //     url: config.api.uri + "/user/login",
-        //     body: "username="+username+"&password="+password
-        // }, function(err, res, body){
-        //     //console.log("username="+username+"&password="+password);
-        //     //console.log(body);
-        //     //res.send();
-        // });
+    app.get('/users/logout', function (req, res, next) {
+        req.session.destroy();
+        res.render('index');
     });
 
-    app.get('/users/logout', function (req, res, next) {
-        //req.session.destroy();
-        res.render('index');
+    app.get('/users/:user', function (req, res, next) {
+        var userID = req.params.user;
+        User.findOne({ _id: userID }, function(err, user) {
+            if(err) return next(err);
+
+            var nickname = user.nickname == "" || user.nickname == null ? user.username : user.nickname;
+            var user = req.session.login_user;
+            user.n_name = nickname;
+            req.session.login_user = user;
+
+            res.render('user', { n_name: nickname });
+        });
     });
 };
