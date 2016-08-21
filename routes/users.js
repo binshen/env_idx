20cefce6ce5012750b9e4e4ed56297499144b980
bin.shen <bin.shen@openx.com>
2016-08-21 09:48:20 +0800

    111

--- routes/users.js
+++ routes/users.js
@@ -13,15 +13,17 @@ module.exports = function (app, mongoose, config) {
     app.get('/users/nickname', function (req, res, next) {
         var user = req.session.login_user;
         var nickname = user.n_name == "" || user.n_name == null ? user.u_name : user.n_name;
-        res.render('user_nickname', { n_name: nickname, u_id: user.u_id });
+        res.render('user_nickname', { u_id: user.u_id, n_name: nickname });
     });
 
     app.get('/users/password', function (req, res, next) {
-        res.render('user_password');
+        var user = req.session.login_user;
+        res.render('user_password', { u_id: user.u_id });
     });
 
     app.get('/users/feedback', function (req, res, next) {
-        res.render('user_feedback');
+        var user = req.session.login_user;
+        res.render('user_feedback', { u_id: user.u_id });
     });
 
     app.get('/users/logout', function (req, res, next) {
