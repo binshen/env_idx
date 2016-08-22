7b9ca368664d02f560dab2d2e5e9c23c9a991eba
bin.shen <bin.shen@openx.com>
2016-08-22 13:51:52 +0800

    111

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -12,6 +13,7 @@ var expressPromise = require('express-promise');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 //var MongoStore = require('connect-mongo')(session);
+var FileStreamRotator = require('file-stream-rotator');
 
 var app = express();
 
@@ -21,7 +23,20 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
+var logDirectory = path.join(__dirname, 'log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+var accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory, 'access-%DATE%.log'),
+  frequency: 'daily',
+  verbose: false
+});
+app.use(logger('combined', {
+  stream: accessLogStream,
+  skip: function (req, res) { return res.statusCode < 400 }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -77,20 +92,20 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // res.status(err.status || 500);
+  res.status(err.status || 500);
   // res.render('error', {
   //   message: err.message,
   //   error: {}
